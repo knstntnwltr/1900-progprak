e17d90837f1f196037116a959eaf15ce56589e44
Elisa Marku <em@hellofresh.com>
2019-05-10 17:25:34 +0200

    adding pages

--- ha02/todo/app/urls.py
+++ ha02/todo/app/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('pages/index.html', views.index),
+    path('pages/add.html', views.add),
+    path('pages/edit.html', views.edit),
+    path('pages/about.html', views.about),
 ]
