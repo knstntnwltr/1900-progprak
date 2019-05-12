cdd7c35bedb14b7f72a4c33a8f4bc2069493568b
Konstantin Walter <k.walter@campus.tu-berlin.de>
2019-05-12 13:30:45 +0200

    Templates and functionality

--- ha02/todo/app/views.py
+++ ha02/todo/app/views.py
@@ -1,7 +1,48 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, HttpResponse, HttpResponseRedirect
+from .models import Todo
+from .forms import TodoForm
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the app index.")
+    c = {'todos': Todo.objects.all()}
+    return render(request, 'index.html', c)
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def add(request):
+    todo = Todo()
+    todo.text = ""
+    todo.date = ""
+    todo.percent = 0
+    todo.save()
+    return render(request, 'edit.html', {'todo': todo})
+
+
+def edit(request, todo_id):
+    todo = get_object_or_404(Todo, pk=todo_id)
+    return render(request, 'edit.html', {'todo': todo})
+
+
+def update(request, todo_id):
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            todo = Todo.objects.get(pk=todo_id)
+            todo.text = form.cleaned_data.get('text')
+            todo.date = form.cleaned_data.get('date')
+            todo.percent = form.cleaned_data.get('percent')
+            todo.save()
+            return HttpResponseRedirect('/')
+        return HttpResponse('not valid, {}'.format(request.POST))
+    else:
+        return HttpResponse('not POST')
+
+
+def delete(request, todo_id):
+    todo = Todo.objects.get(pk=todo_id)
+    todo.delete()
+    return HttpResponseRedirect('/')
